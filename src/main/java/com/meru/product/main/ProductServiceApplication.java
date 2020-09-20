f8f9ec7654344b467ef0cee67a589ebeb7b3f2b8
sanjeev <sanjeev321@gmail.com>
2020-09-20 19:29:41 +0100

    removed eureka

--- src/main/java/com/meru/product/main/ProductServiceApplication.java
+++ src/main/java/com/meru/product/main/ProductServiceApplication.java
@@ -3,10 +3,8 @@ package com.meru.product.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
 @EntityScan("com.meru.product.model")
 @EnableJpaRepositories("com.meru.product.dao")
 @SpringBootApplication(scanBasePackages = {"com.meru"})
